ada2144d7d217795a5ee315843fcd0d764f90fa9
Maxwell <maxwellpires2002@gmail.com>
2024-04-16 13:23:58 -0300

    Update 2022_J

--- Maratona_2022/J/J.py
+++ Maratona_2022/J/J.py
@@ -10,35 +10,35 @@ m = 0
 
 #Somamos pontos de João e marcamos a carta que foi usada
 c1, c2 = map(int, (input().split()))
-j += c1 
-j+=c2
-usadas[c1]+=1
-usadas[c2]+=1
+j += cartas[c1-1]
+j += cartas[c2-1]
+usadas[c1-1]+=1
+usadas[c2-1]+=1
 
 #Somamos pontos de Maria e marcamos a carta que foi usada
 c1, c2 = map(int, (input().split()))
-m += c1 
-m +=c2
-usadas[c1]+=1
-usadas[c2]+=1
+m += cartas[c1-1]
+m += cartas[c2-1]
+usadas[c1-1]+=1
+usadas[c2-1]+=1
 
 #Guardamos as proximas cartas, que são comuns
 next_j = list(map(int, input().split()))
 
 #Somamos os pontos comuns e marcamos como cartas usadas
 for i in range(n):
-    j += next_j[i]
-    m += next_j[i]
-    usadas[next_j[i]] += 1
+    j += cartas[next_j[i]-1]
+    m += cartas[next_j[i]-1]
+    usadas[next_j[i]-1] += 1
 
 #Verificamos da menor carta para a maior em pontuação qual faz Maria vencer
 for i in range(len(usadas)):
     #Maria vence quando faz exatos 23 pontos indendente dos pontos de João, ou quando João estoura e ela não
     if ((usadas[i] < 4) and ((m + cartas[i] == 23) or ((j + cartas[i] > 23) and (m + cartas[i] < 23)))):
-        print(i)
+        print(i+1)
         maria_vence = 1 #Maria venceu
         break
-#Se Maria não venceu imprimimos -1
+#Se Maria não venceu imprimimos
 if(not(maria_vence)):
    print(-1) 
 
