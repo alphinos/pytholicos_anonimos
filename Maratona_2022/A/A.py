b9b46be798a2b7312d8df7237b414ded5fe4da2d
Maxwell <maxwellpires2002@gmail.com>
2024-04-16 13:10:39 -0300

    Coments Update

--- Maratona_2022/A/A.py
+++ Maratona_2022/A/A.py
@@ -1,9 +1,10 @@
 #Questão A
 a = int(input())
 s = input()
-s = f"b{s}b"
+s = f"b{s}b" #Mesma string da entrada com 'b' no inicio e fim
 ans =  0
-for i in range(2, a+2):
+for i in range(2, a+2): #For do segundo elemento até o ultimo da string
+    #Quando tivermos a subpaalbra 'aa' ou 'aab' contaremos como um a. Ao final a contagem será correta
     if((s[i] == 'a' and s[i-1] == 'a')or(s[i] == 'b' and s[i-1] == 'a' and s[i-2] == 'a')):
         ans+=1
 print(ans)
--- Maratona_2022/J/J.py
+++ Maratona_2022/J/J.py
@@ -1,39 +1,44 @@
 #Questão J
 n = int(input())
 
-cartas = [1,2,3,4,5,6,7,8,9,10,10,10,10]
-usadas = [0 for i in range(13)]
-maria_vence = 0
+cartas = [1,2,3,4,5,6,7,8,9,10,10,10,10] #Cartas e suas pontuações no respectivo índice
+usadas = [0 for i in range(13)] #Armazenas quais cartas usadas. Só existem 4 cartas de cada
+maria_vence = 0 
 
 j = 0
 m = 0
 
+#Somamos pontos de João e marcamos a carta que foi usada
 c1, c2 = map(int, (input().split()))
 j += c1 
 j+=c2
 usadas[c1]+=1
 usadas[c2]+=1
 
-
+#Somamos pontos de Maria e marcamos a carta que foi usada
 c1, c2 = map(int, (input().split()))
 m += c1 
 m +=c2
 usadas[c1]+=1
 usadas[c2]+=1
 
+#Guardamos as proximas cartas, que são comuns
 next_j = list(map(int, input().split()))
 
+#Somamos os pontos comuns e marcamos como cartas usadas
 for i in range(n):
     j += next_j[i]
     m += next_j[i]
     usadas[next_j[i]] += 1
-    
-for i in range(len(usadas)):
-    if ((usadas[i] < 4) and ((m + i == 23) or ((j + i > 23) and (m + i <= 23)))):
-        print(i)
-        maria_vence = 1
-        break
 
+#Verificamos da menor carta para a maior em pontuação qual faz Maria vencer
+for i in range(len(usadas)):
+    #Maria vence quando faz exatos 23 pontos indendente dos pontos de João, ou quando João estoura e ela não
+    if ((usadas[i] < 4) and ((m + cartas[i] == 23) or ((j + cartas[i] > 23) and (m + cartas[i] < 23)))):
+        print(i)
+        maria_vence = 1 #Maria venceu
+        break
+#Se Maria não venceu imprimimos -1
 if(not(maria_vence)):
    print(-1) 
 
