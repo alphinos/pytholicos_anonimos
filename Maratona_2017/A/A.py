5ef2867ed3c62564a44cf3da50d2684900c1685e
Max <maxwellpires2002@gmail.com>
2024-05-24 20:08:38 -0300

    2017 A Update - SegTree

--- Maratona_2017/A/A.py
+++ Maratona_2017/A/A.py
@@ -9,100 +9,89 @@ lazy = [0 for i in range(4*N)]
 
 freqs = { 1 : n for n in range( N ) }
 
+print(S)
+
+ELEMENTO_NEUTRO = [[],[]]
+
 #Build
 def freq(l, r):
-    # L_k = set(l.keys())
-    # R_k = set(r.keys())
 
-    # cu = l.keys() + r.keys()
-    # for e in cu:
-    #     pass
+    chaves_l = set(l[0])
+    chaves_r = set(r[0])
 
-    # freq_dict = {}
-    # if L_k == R_k:
-    #     for k in L_k:
-    #         if not(L_k[k] & R_k[k]):
-    #             freq_dict[k] = L_k[k] | R_k[k]
-    #         else:
-    #             uniao = L_k[k] | R_k[k]
-    #             interc = L_k[k] & R_k[k]
-    #             freq_dict[k] = (uniao - interc)
-    #             freq_dict[k+1] = interc
+    intersec = chaves_l & chaves_r
+
+    resposta = [[], []]
+
+    for k in intersec:
+        i_l= l[0].index(k)
+        i_r = r[0].index(k)
+        resposta[0].append(k)
+        resposta[1].append(l[1][i_l]+r[1][i_r])
     
-                
-                
-            
-    if l[1] & r[1]:
-        return (l[0]+r[0], l[1] & r[1])
-
-
-    if l[0] == r[0] and not(l[1] & r[1]):
-        return (l[0], l[1] | r[1])
+    for k in chaves_r - intersec:
+        i_r= r[0].index(k)
+        resposta[0].append(k)
+        resposta[1].append(r[1][i_r])
     
-    
+    for k in chaves_l - intersec:
+        i_l= l[0].index(k)
+        resposta[0].append(k)
+        resposta[1].append(l[1][i_l])
 
-    return l if l[0] > r[0] else r
+    
+    return resposta
     
 
 def build(p: int, l: int, r: int):
 
     if l == r:
-        S[p] = (1, set([V[l]]))
+        S[p] = [[V[l]], [1]]
         return S[p]
     
     m = (l+r)//2
     
     freq_set = freq(build(2*p, l, m), build(2*p+1, m+1, r))
-
-    if p == 1:
-        S[p] = (freq_set[0], max(freq_set[1]))
-    else:
-        S[p] = freq_set
-
+    S[p] = freq_set 
     return S[p]
 
 
-
-
 def query(a: int, b:int, p:int, l:int, r:int):
-    # print(S)
+
     if lazy[p] != 0:
-        S[p] = ( S[p][0], {valor + lazy[p] for valor in S[p][1]})
+        tam = len(S[p][0])
+        for i in range(tam):
+            S[p][0][i] += lazy[p]
+
         lazy[p] = 0
 
     if b < l or a > r:
-        return (0, set())
+        return ELEMENTO_NEUTRO
     
     if a <= l and r <= b:
         return S[p]
     
     m = (l+r)//2
-    # print(m)
     freq_set = freq(query(a, b, 2*p, l, m), query(a, b, 2*p+1, m+1, r))
-
-    if p == 1:
-        return (freq_set[0], max(freq_set[1]))
-    else:
-        return freq_set
+    S[p] = freq_set 
+    return S[p]
 
     
-
-
-
 def update(a: int, b:int,  v: int, p: int, l: int, r: int):
  
     if lazy[p] != 0:
-        S[p] = ( S[p][0], {valor + lazy[p] for valor in S[p][1]})
+        tam = len(S[p][0])
+        for i in range(tam):
+            S[p][0][i] += lazy[p]
         lazy[p] = 0
 
     if a > r or b < l:
         return S[p]
     
     if l >= a and r <= b:
-        if( p != 1):
-            S[p] = ( S[p][0], {valor + v for valor in S[p][1]})
-        else:
-            S[p] = ( S[p][0], S[p][1] + v)
+        tam = len(S[p][0])
+        for i in range(tam):
+            S[p][0][i] += v
 
         lazy[2*p] += v
         lazy[2*p+1] += v
@@ -110,13 +99,8 @@ def update(a: int, b:int,  v: int, p: int, l: int, r: int):
     
     m = (l+r)//2
 
-    freq_set =  freq(update(a, b, v, 2*p, l, m), update(a, b, v, 2*p+1, m+1, r))
-
-    if p == 1:
-        S[p] = (freq_set[0], max(freq_set[1]))
-    else:
-        S[p] = freq_set
-
+    freq_set = freq(update(a, b, v, 2*p, l, m), update(a, b, v, 2*p+1, m+1, r))
+    S[p] = freq_set 
     return S[p]
 
 build(1, 0, N-1)
@@ -124,13 +108,17 @@ build(1, 0, N-1)
 for _ in range( Q ):
     a, b = map( int, input().split())
     print(S)
-    frequencia = query(a, b, 1, 0, N-1)[1]
-    update(a, b, frequencia, 1, 0, N-1)
+    ans = query(a, b, 1, 0, N-1)
+    max_freq = max(ans[1])
+    idx = ans[1].index(max_freq)
+    frecx = ans[0][idx]
+    update(a, b, frecx, 1, 0, N-1)
     print(S)
 
 
+def visita_folhas(p:int, l:int , r:int):
+    pass
 
-print(S)
 
 
 
