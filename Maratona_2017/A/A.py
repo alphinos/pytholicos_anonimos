f966c6d66b688553243259b2c4396d67488f427b
Max <maxwellpires2002@gmail.com>
2024-05-24 23:10:44 -0300

    TLE 2017 A De Novo Q bomba

--- Maratona_2017/A/A.py
+++ Maratona_2017/A/A.py
@@ -9,13 +9,14 @@ lazy = [0 for i in range(4*N)]
 
 freqs = { 1 : n for n in range( N ) }
 
-print(S)
-
 ELEMENTO_NEUTRO = [[],[]]
+
+global resposta
 
 #Build
 def freq(l, r):
 
+    global resposta
     chaves_l = set(l[0])
     chaves_r = set(r[0])
 
@@ -27,7 +28,7 @@ def freq(l, r):
         i_l= l[0].index(k)
         i_r = r[0].index(k)
         resposta[0].append(k)
-        resposta[1].append(l[1][i_l]+r[1][i_r])
+        resposta[1].append((l[1][i_l]+r[1][i_r]))
     
     for k in chaves_r - intersec:
         i_r= r[0].index(k)
@@ -39,9 +40,11 @@ def freq(l, r):
         resposta[0].append(k)
         resposta[1].append(l[1][i_l])
 
-    
+   
     return resposta
-    
+
+
+
 
 def build(p: int, l: int, r: int):
 
@@ -61,7 +64,10 @@ def query(a: int, b:int, p:int, l:int, r:int):
     if lazy[p] != 0:
         tam = len(S[p][0])
         for i in range(tam):
-            S[p][0][i] += lazy[p]
+            S[p][0][i] = (lazy[p] + S[p][0][i])%9
+
+        lazy[2*p] += lazy[p]
+        lazy[2*p+1] += lazy[p]
 
         lazy[p] = 0
 
@@ -82,7 +88,10 @@ def update(a: int, b:int,  v: int, p: int, l: int, r: int):
     if lazy[p] != 0:
         tam = len(S[p][0])
         for i in range(tam):
-            S[p][0][i] += lazy[p]
+            S[p][0][i] = (lazy[p] + S[p][0][i])%9
+
+        lazy[2*p] += lazy[p]
+        lazy[2*p+1] += lazy[p]
         lazy[p] = 0
 
     if a > r or b < l:
@@ -91,10 +100,11 @@ def update(a: int, b:int,  v: int, p: int, l: int, r: int):
     if l >= a and r <= b:
         tam = len(S[p][0])
         for i in range(tam):
-            S[p][0][i] += v
+            S[p][0][i] = (v + S[p][0][i])%9
 
         lazy[2*p] += v
         lazy[2*p+1] += v
+
         return S[p]
     
     m = (l+r)//2
@@ -107,17 +117,41 @@ build(1, 0, N-1)
 
 for _ in range( Q ):
     a, b = map( int, input().split())
-    print(S)
     ans = query(a, b, 1, 0, N-1)
+    tam = len(ans[0])
     max_freq = max(ans[1])
-    idx = ans[1].index(max_freq)
-    frecx = ans[0][idx]
-    update(a, b, frecx, 1, 0, N-1)
-    print(S)
-
-
+    idx_max = ans[1].index(max_freq)
+    maior_freq = ans[0][idx_max]
+    
+    for k in range(tam):
+        if ans[1][k] == max_freq:
+            if ans[0][k] > maior_freq:
+                maior_freq = ans[0][k]
+                
+    update(a, b, maior_freq, 1, 0, N-1)
+  
+    
 def visita_folhas(p:int, l:int , r:int):
-    pass
+
+    if lazy[p] != 0:
+        tam = len(S[p][0])
+        for i in range(tam):
+            S[p][0][i] = (lazy[p] + S[p][0][i])%9
+
+        lazy[2*p] += lazy[p]
+        lazy[2*p+1] += lazy[p]
+        lazy[p] = 0
+
+    if l == r:
+        print(S[p][0][0])
+        return
+    
+    m = (l+r)//2
+
+    visita_folhas(2*p, l, m)
+    visita_folhas(2*p+1, m+1, r)
+
+visita_folhas(1, 0, N-1)
 
 
 
