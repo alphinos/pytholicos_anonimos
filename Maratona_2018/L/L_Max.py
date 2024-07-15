dac1cb64215c1252f6436a2c12aa35df2fdbe471
Max <maxwellpires2002@gmail.com>
2024-07-15 10:15:41 -0300

    L 2018 - Teste

--- Maratona_2018/L/L_Max.py
+++ Maratona_2018/L/L_Max.py
@@ -11,7 +11,11 @@ class Vertice:
     def mostrar_vertice(self):
         print(f"ID: {self.id}")
         print(f"ADJ: {self.adj}")
-        print(f"PAI: {self.pai}")
+        if self.pai != None:
+            print(f"PAI: {self.pai.id}")
+        else:
+            print("RAIZ - PAI DE TODOS")
+
 
 class Grafo:
     def __init__(self, vertices_n) -> None:
@@ -27,6 +31,11 @@ class Grafo:
             print()
         print("__________________")
     
+    def restart(self):
+        for i in range(self.tam):
+            self.vertices[i].visto = False
+    
+    
     
 
 g = Grafo(N)
@@ -36,17 +45,17 @@ for i in range(N-1):
     g.vertices[E-1].adj.append(S)
     g.vertices[S-1].adj.append(E)
 
-
 def DFS(g: Grafo, A, B):
     encontrado = False
 
     stack = deque()
     stack.append(g.vertices[A-1])
     g.vertices[A-1].visto = True
+    g.vertices[A-1].pai = None
 
     while stack and not encontrado:
         w = stack.pop()
-        print(f"Saiu da pilha {w.id}")
+        #   print(f"Saiu da pilha {w.id}")
         for k in w.adj:
             v = g.vertices[k-1]
             if v.visto == False:
@@ -64,18 +73,41 @@ def DFS(g: Grafo, A, B):
         pontos.append(v.id)
         v = v.pai
 
-    pontos.append(A)    
+    pontos.append(A) 
+    g.restart()   
     return pontos
 
 
 
 def pontos_comuns(g: Grafo, A, B, C, D):
-    pass
+    linha_um = DFS(g, A, B)
+    linha_dois = DFS(g, C, D)  
+    #print(linha_um)
+    #print(linha_dois)
+    linha_um.sort() 
+    linha_dois.sort()
+
+    tam_um = len(linha_um)
+    tam_dois = len(linha_dois)
+
+    one = 0
+    two = 0
+
+    contador = 0
+
+    while one < tam_um and two < tam_dois:
+
+        if linha_um[one] == linha_dois[two]:
+            contador += 1
+            one += 1
+            two += 1
+
+        elif linha_um[one] < linha_dois[two]:
+            one += 1
+        else:
+            two += 1
     
-
-g.mostrar_grafo()
-print(DFS(g, 2, 4))
-
+    return contador
 
 for i in range(Q):
     A, B, C, D = map(int, input().split())
